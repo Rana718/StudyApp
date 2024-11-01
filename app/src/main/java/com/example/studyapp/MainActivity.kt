d0019ca68503ad37b333214a497fa559d27f85b5
Rana718 <ranadolui718@gmail.com>
2024-11-01 17:54:05 +0530

    Add Sesson and Service logic

--- app/src/main/java/com/example/studyapp/MainActivity.kt
+++ app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -1,24 +1,84 @@
 package com.example.studyapp
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.app.ActivityCompat
+import com.example.studyapp.service.StudySessionTimerService
 import com.example.studyapp.ui.NavGraphs
+import com.example.studyapp.ui.destinations.SessionScreenRouteDestination
 import com.example.studyapp.ui.theme.StudyAppTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.navigation.dependency
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private var isBound by mutableStateOf(false)
+    private lateinit var timerService: StudySessionTimerService
+
+    private val connection = object : ServiceConnection{
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as StudySessionTimerService.StudySessionTimerBinder
+            timerService = binder.getService()
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBound = false
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, StudySessionTimerService::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            StudyAppTheme {
-                DestinationsNavHost(navGraph = NavGraphs.root)
+            if(isBound){
+                StudyAppTheme {
+                    DestinationsNavHost(
+                        navGraph = NavGraphs.root,
+                        dependenciesContainerBuilder = {
+                            dependency(SessionScreenRouteDestination) { timerService }
+                        }
+                    )
+                }
             }
         }
+        requestPermission()
+    }
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        isBound = false
     }
 }
 
--- app/src/main/java/com/example/studyapp/ui/session/SessionScreen.kt
+++ app/src/main/java/com/example/studyapp/ui/session/SessionScreen.kt
@@ -1,5 +1,14 @@
 package com.example.studyapp.ui.session
 
+import android.content.Intent
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,15 +24,19 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,26 +45,55 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.studyapp.service.ServiceHelper
+import com.example.studyapp.service.StudySessionTimerService
 import com.example.studyapp.ui.components.DeletePopup
 import com.example.studyapp.ui.components.SubjectListBottom
 import com.example.studyapp.ui.components.studySessionList
+import com.example.studyapp.ui.theme.Red
+import com.example.studyapp.util.Constants.ACTION_SERVICE_CANCEL
+import com.example.studyapp.util.Constants.ACTION_SERVICE_START
+import com.example.studyapp.util.Constants.ACTION_SERVICE_STOP
+import com.example.studyapp.util.SnackbarEvent
+import com.example.studyapp.util.TimerState
+import com.ramcosta.composedestinations.annotation.DeepLink
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlin.time.DurationUnit
 
 
-@Destination
+@Destination(
+    deepLinks = [
+        DeepLink(
+            action = Intent.ACTION_VIEW,
+            uriPattern = "study_app://dashboard/session"
+        )
+    ]
+)
+
 @Composable
 fun SessionScreenRoute(
-    navigator: DestinationsNavigator
+    navigator: DestinationsNavigator,
+    timerService: StudySessionTimerService
 ){
 
     val viewModel: SessionViewModel = hiltViewModel()
+    val state by viewModel.state.collectAsStateWithLifecycle()
 
     SessionScreen(
+        state = state,
+        snackbarEvent = viewModel.snackbarEventFlow,
+        onEvent = viewModel::onEvent,
+        timerService = timerService,
         onBackButtonClick = { navigator.navigateUp() }
     )
 }
@@ -60,24 +102,58 @@ fun SessionScreenRoute(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun SessionScreen(
+    state: SessionState,
+    snackbarEvent: SharedFlow<SnackbarEvent>,
+    onEvent: (SessionEvent) -> Unit,
+    timerService: StudySessionTimerService,
     onBackButtonClick: () -> Unit
 ){
-
+    val hours by timerService.hours
+    val minutes by timerService.minutes
+    val seconds by timerService.seconds
+    val currentTimerState by timerService.currentTimerState
+    val context = LocalContext.current
     val scope = rememberCoroutineScope()
     val sheetState = rememberModalBottomSheetState()
     var isBottomSheetOpen by remember { mutableStateOf(false) }
-
     var isDeletePopupOpen by rememberSaveable { mutableStateOf(false) }
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(key1 = true) {
+        snackbarEvent.collectLatest { event ->
+            when (event) {
+                is SnackbarEvent.ShowSnackbar -> {
+                    snackbarHostState.showSnackbar(
+                        message = event.message,
+                        duration = event.duration
+                    )
+                }
+
+                SnackbarEvent.NavigateUp -> {}
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = state.subjects) {
+        val subjectId = timerService.subjectId.value
+        onEvent(
+            SessionEvent.UpdateSubjectIdAndRelatedSubject(
+                subjectId = subjectId,
+                relatedToSubject = state.subjects.find { it.subjectId == subjectId }?.name
+            )
+        )
+    }
 
     SubjectListBottom (
         sheetState = sheetState,
         isOpen = isBottomSheetOpen,
-        subjects = ,
+        subjects = state.subjects,
         onDismiss = { isBottomSheetOpen = false},
-        onSubjectClick = {
+        onSubjectClick = { subject ->
             scope.launch { sheetState.hide() }.invokeOnCompletion {
                 if (!sheetState.isVisible) isBottomSheetOpen = false
             }
+            onEvent(SessionEvent.OnRelatedSubjectChange(subject))
         }
 
     )
@@ -87,10 +163,14 @@ private fun SessionScreen(
         title = "Delete Subject",
         bodyText = "Are you sure you want to delete this subject?",
         onDismiss = { isDeletePopupOpen = false },
-        onConfirm = { isDeletePopupOpen = false }
+        onConfirm = {
+            onEvent(SessionEvent.DeleteSession)
+            isDeletePopupOpen = false
+        }
     )
 
     Scaffold (
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             SessionScreenTopBar(onBackButtonClick = onBackButtonClick)
         }
@@ -100,29 +180,64 @@ private fun SessionScreen(
         ){
             item{
                 TimerSection(
-                    modifier = Modifier.fillMaxWidth().aspectRatio(1f)
+                    modifier = Modifier.fillMaxWidth().aspectRatio(1f),
+                    hours = hours,
+                    minutes = minutes,
+                    seconds = seconds
                 )
             }
             item{
                 RelatedToSubjectSection(
                     modifier = Modifier.fillMaxWidth().padding(horizontal = 12.dp),
-                    relatedToSubject = "English",
-                    selectSubjectButtonClick = { isBottomSheetOpen = true}
+                    relatedToSubject = state.relatedToSubject ?: "",
+                    selectSubjectButtonClick = { isBottomSheetOpen = true},
+                    seconds = seconds
                 )
             }
             item{
                 ButtonSection(
                     modifier = Modifier.fillMaxWidth().padding(12.dp),
-                    startButtonClick = {},
-                    cancelButtonClick = {},
-                    finishButtonClick = {}
+                    startButtonClick = {
+                        if (state.subjectId != null && state.relatedToSubject != null) {
+                            ServiceHelper.triggerForegroundService(
+                                context = context,
+                                action = if (currentTimerState == TimerState.STARTED) {
+                                    ACTION_SERVICE_STOP
+                                } else ACTION_SERVICE_START
+                            )
+                            timerService.subjectId.value = state.subjectId
+                        } else {
+                            onEvent(SessionEvent.NotifyToUpdateSubject)
+                        }
+                    },
+                    cancelButtonClick = {
+                        ServiceHelper.triggerForegroundService(
+                            context = context,
+                            action = ACTION_SERVICE_CANCEL
+                        )
+                    },
+                    finishButtonClick = {
+                        val duration = timerService.duration.toLong(DurationUnit.SECONDS)
+                        if (duration >= 36) {
+                            ServiceHelper.triggerForegroundService(
+                                context = context,
+                                action = ACTION_SERVICE_CANCEL
+                            )
+                        }
+                        onEvent(SessionEvent.SaveSession(duration))
+                    },
+                    timerState = currentTimerState,
+                    seconds = seconds
                 )
             }
             studySessionList(
                 title = "Recent Study Sessions",
                 textList = "You Don't have any recent Study Sessions",
-                sessions = ,
-                onDeleteIconClick = {isDeletePopupOpen=true}
+                sessions = state.sessions,
+                onDeleteIconClick = { session ->
+                    isDeletePopupOpen=true
+                    onEvent(SessionEvent.OnDeleteSessionButtonClick(session))
+                }
             )
         }
     }
@@ -151,7 +266,10 @@ private fun SessionScreenTopBar(
 
 @Composable
 private fun TimerSection(
-    modifier: Modifier
+    modifier: Modifier,
+    hours: String,
+    minutes: String,
+    seconds: String
 ){
     Box(
         modifier = modifier,
@@ -161,10 +279,38 @@ private fun TimerSection(
             modifier = Modifier.size(250.dp)
                 .border(5.dp, MaterialTheme.colorScheme.surfaceVariant, CircleShape)
         )
-        Text(
-            text = "00:00:00",
-            style = MaterialTheme.typography.titleLarge.copy(fontSize = 45.sp)
-        )
+        Row{
+            AnimatedContent(
+                targetState = hours,
+                label = hours,
+                transitionSpec = { timerTextAnimation() }
+            ) { hours ->
+                Text(
+                    text = "$hours:",
+                    style = MaterialTheme.typography.titleLarge.copy(fontSize = 45.sp)
+                )
+            }
+            AnimatedContent(
+                targetState = minutes,
+                label = minutes,
+                transitionSpec = { timerTextAnimation() }
+            ) { minutes ->
+                Text(
+                    text = "$minutes:",
+                    style = MaterialTheme.typography.titleLarge.copy(fontSize = 45.sp)
+                )
+            }
+            AnimatedContent(
+                targetState = seconds,
+                label = seconds,
+                transitionSpec = { timerTextAnimation() }
+            ) { seconds ->
+                Text(
+                    text = seconds,
+                    style = MaterialTheme.typography.titleLarge.copy(fontSize = 45.sp)
+                )
+            }
+        }
     }
 }
 
@@ -174,25 +320,44 @@ private fun ButtonSection(
     modifier: Modifier,
     startButtonClick: () -> Unit,
     cancelButtonClick: () -> Unit,
-    finishButtonClick: () -> Unit
+    finishButtonClick: () -> Unit,
+    timerState: TimerState,
+    seconds: String
 ){
     Row (
         modifier = modifier,
         horizontalArrangement = Arrangement.SpaceBetween
     ){
-        Button(onClick = cancelButtonClick) {
+        Button(
+            onClick = cancelButtonClick,
+            enabled = seconds != "00" && timerState != TimerState.STARTED
+        ) {
             Text(
                 modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp),
                 text = "Cancel"
             )
         }
-        Button(onClick = startButtonClick) {
+        Button(
+            onClick = startButtonClick,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = if (timerState == TimerState.STARTED) Red
+                else MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            )
+        ) {
             Text(
                 modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp),
-                text = "Start"
+                text = when(timerState){
+                    TimerState.STARTED -> "Stop"
+                    TimerState.STOPPED -> "Resume"
+                    else -> "Start"
+                }
             )
         }
-        Button(onClick = finishButtonClick) {
+        Button(
+            onClick = finishButtonClick,
+            enabled = seconds != "00" && timerState != TimerState.STARTED
+        ) {
             Text(
                 modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp),
                 text = "Finish"
@@ -206,7 +371,8 @@ private fun ButtonSection(
 private fun RelatedToSubjectSection(
     modifier: Modifier,
     relatedToSubject: String,
-    selectSubjectButtonClick: () -> Unit
+    selectSubjectButtonClick: () -> Unit,
+    seconds: String,
 ){
     Column (modifier = modifier){
         Text(
@@ -222,7 +388,7 @@ private fun RelatedToSubjectSection(
                 text = relatedToSubject,
                 style = MaterialTheme.typography.bodyLarge
             )
-            IconButton(onClick =  selectSubjectButtonClick ) {
+            IconButton(onClick =  selectSubjectButtonClick, enabled = seconds == "00" ) {
                 Icon(
                     imageVector = Icons.Default.ArrowDropDown,
                     contentDescription = "Select Subject"
@@ -230,4 +396,13 @@ private fun RelatedToSubjectSection(
             }
         }
     }
+}
+
+
+private fun timerTextAnimation(duration: Int = 600): ContentTransform {
+    return slideInVertically(animationSpec = tween(duration)) { fullHeight -> fullHeight } +
+            fadeIn(animationSpec = tween(duration)) togetherWith
+            slideOutVertically(animationSpec = tween(duration)) { fullHeight -> -fullHeight } +
+            fadeOut(animationSpec = tween(duration))
+
 }
